7980d9df5015c3aa243cf4e44f775db198984e65
Divya Vellanki <divya.vellanki94@gmail.com>
2025-03-26 23:59:54 -0400

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,1 +1,7 @@
+/*
+This is a stored procedure to BULK LOAD (all at once) instead of LOAD (row by row) from source systems (CSV files) to tables in bronze schema
+naming format: load_bronze;
 
+Usage example:
+  EXEC load.bronze;
+*/
